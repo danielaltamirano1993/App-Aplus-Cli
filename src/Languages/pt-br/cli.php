7909aa274fbcaf23efb268662e4d1a6a32ed32f5
danielaltamirano1993 <64813513+danielaltamirano1993@users.noreply.github.com>
2022-10-17 16:37:50 -0500

    Create cli.php

---
+++ src/Languages/pt-br/cli.php
@@ -0,0 +1,31 @@
+<?php
+/*
+ * This file is part of Aplus Framework CLI Library.
+ *
+ * (c) Natan Felles <natanfelles@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+return [
+    'about.description' => 'Mostra informações sobre essa interface de linha de comandos.',
+    'about.line1' => 'Sobre',
+    'about.line2' => 'Essa interface é construída sobre Aplus Framework CLI Library.',
+    'about.line3' => 'Aplus Framework CLI Library é Software de Código Aberto (OSS).',
+    'about.line4' => 'Visite nosso website para saber mais: https://aplus-framework.com',
+    'about.line5' => 'Obrigado por usar o Aplus Framework!',
+    'command' => 'Comando',
+    'commandNotFound' => 'Comando não encontrado: "{0}"',
+    'commands' => 'Comandos',
+    'description' => 'Descrição',
+    'friend' => 'amigo',
+    'greet.afternoon' => 'Boa tarde, {0}!',
+    'greet.evening' => 'Boa noite, {0}!',
+    'greet.morning' => 'Bom dia, {0}!',
+    'help.description' => 'Mostra a ajuda de uso do comando.',
+    'index.description' => 'Mostra a lista de comandos.',
+    'index.option.greet' => 'Mostra saudação.',
+    'noDescription' => 'Este comando não fornece uma descrição.',
+    'options' => 'Opções',
+    'usage' => 'Uso',
+];
