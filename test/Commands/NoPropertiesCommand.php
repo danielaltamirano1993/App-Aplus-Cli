93d0b20888ab30f62ced352fda38933f6047d538
danielaltamirano1993 <64813513+danielaltamirano1993@users.noreply.github.com>
2022-10-20 19:06:12 -0500

    Create NoPropertiesCommand.php

---
+++ test/Commands/NoPropertiesCommand.php
@@ -0,0 +1,20 @@
+<?php
+/*
+ * This file is part of Aplus Framework CLI Library.
+ *
+ * (c) Natan Felles <natanfelles@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Tests\CLI\Commands;
+
+use Framework\CLI\Command;
+
+class NoPropertiesCommand extends Command
+{
+    public function run() : void
+    {
+        //
+    }
+}
